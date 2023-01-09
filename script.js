5f14d21a90f7fd22f01eeda4c111396e00cd06a0
Connie Wong <conniehhw@gmail.com>
2023-01-09 16:26:12 -0500

    Set up .js, .html, .css & API variables

---
+++ script.js
@@ -0,0 +1,7 @@
+var APIKey = "dbc89065a7085f3e04b8795339fe050e";
+
+var city;
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+
+var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}";
