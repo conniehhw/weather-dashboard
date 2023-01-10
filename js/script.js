129f95d2d3211f3106cc110d28bef56e3f29016c
Connie Wong <conniehhw@gmail.com>
2023-01-10 15:49:57 -0500

    Test fetch route for Weather API

--- js/script.js
+++ js/script.js
@@ -6,27 +6,35 @@
 
 // // var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}";
 
-var repoList = document.querySelector('ul');
-var searchButton = document.getElementById('search-button');
+// var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}";
 
-function getApi() {
+
+// var repoList = document.querySelector('ul');
+// var searchButton = document.getElementById('search-button');
+
+// function getApi() {
   // replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'https://api.github.com/users/octocat/repos';
+//   var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?';
+  var requestUrl = 
+  
+  
+  'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=dbc89065a7085f3e04b8795339fe050e';
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data)  {
-    console.log(data);  {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
-    }
+    console.log(data);  
+//     {
+//       for (var i = 0; i < data.length; i++) {
+//         var listItem = document.createElement('li');
+//         listItem.textContent = data[i].html_url;
+//         repoList.appendChild(listItem);
+//       }
+//     }
+// });
 });
-};
     
 
-searchButton.addEventListener('click', getApi);
+// searchButton.addEventListener('click', getApi);
