3d14791ca716630eadefd4eb3ee562759684e9d9
Connie Wong <conniehhw@gmail.com>
2023-01-12 11:50:06 -0500

    Add date function to city result container

--- js/script.js
+++ js/script.js
@@ -32,13 +32,29 @@ var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=
 
     var cityName = document.createElement('h1'); //addClass / set Attribute to style to bold?
 
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+//let date = new Date().toLocaleDateString("de-DE"); get time in Germany
+// console.log(date); // 17.6.2022
+//https://saimana.com/list-of-country-locale-code/
+
+    let currentDate = `${day}/${month}/${year}`;
+    console.log(currentDate);
+
     var tempContentEl = document.createElement('p');
     tempContentEl.innerHTML =
-      '<strong>Temp:</strong> ' + data.main.temp;
+      '<strong>Temp:</strong> ' + data.main.temp + ' F/C';
 
     var windContentEl = document.createElement('p');
     windContentEl.innerHTML =
-      '<strong>Wind Speed:</strong> ' + data.wind.speed;
+      '<strong>Wind Speed:</strong> ' + data.wind.speed + ' MPH';
+
+      var humidityContentEl = document.createElement('p');
+    humidityContentEl.innerHTML =
+      '<strong>Humidity:</strong> ' + data.main.humidity + ' %';
 
     cityName.textContent = data.name;
     // cityTemp.textContent = data.main.temp;
@@ -48,9 +64,10 @@ var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=
       // resultContainer.append(cityTemp);
       // resultContainer.append(weatherDescription);
 
-      resultContainer.append(cityName);
+      resultContainer.append(cityName, currentDate);
       resultContainer.append(tempContentEl);
       resultContainer.append(windContentEl);
+      resultContainer.append(humidityContentEl);
     
 //     {
 //       for (var i = 0; i < data.length; i++) {
