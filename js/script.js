a34e675e7bd1de0a092bd2b351a0dcb3829acd31
Connie Wong <conniehhw@gmail.com>
2023-01-12 16:57:50 -0500

    Get API data for 5 day forecast, display in daily box

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ var searchButton = document.getElementById('search-button');
 
 searchButton.addEventListener('click', getApi);
 
+
 function getApi() {
 
 var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=dbc89065a7085f3e04b8795339fe050e&units=imperial';
@@ -52,7 +53,7 @@ var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=
 
     var windContentEl = document.createElement('p');
     windContentEl.innerHTML =
-      '<strong>Wind Speed:</strong> ' + data.wind.speed + ' MPH';
+      '<strong>Wind:</strong> ' + data.wind.speed + ' MPH';
 
       var humidityContentEl = document.createElement('p');
     humidityContentEl.innerHTML =
@@ -70,9 +71,10 @@ var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=
       resultContainer.append(tempContentEl);
       resultContainer.append(windContentEl);
       resultContainer.append(humidityContentEl);
-    
-});
+  });
 }
+  
+
 //     {
 //       for (var i = 0; i < data.length; i++) {
 //         var listItem = document.createElement('li');
@@ -83,20 +85,90 @@ var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=
 // });
 
 //////////////// API call for 5 day forecast
-var resultContainer = document.getElementById('forecast');
-searchButton.addEventListener('click', getApi);
+// var resultContainer1 = document.getElementById('forecast');
+// searchButton.addEventListener('click', getApi1);
 
-function getApi() {
+// function getApi1() {
 
-var requestFiveDayUrl =
-'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&cnt=40&appid=9f768ebe1fd823ba79ca13f691dcc877&units=imperial';
+// var requestFiveDayUrl ='https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&cnt=40&appid=9f768ebe1fd823ba79ca13f691dcc877&units=imperial';
+
+//   fetch(requestFiveDayUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data)  {
+//     console.log(data);  
+//     })
+  
+
+//   var cityName1 = document.createElement('h1'); //addClass / set Attribute to style to bold?
+  
+//   cityName1.textContent = data.name;
+//   resultContainer1.append(cityName1);
+// }
+
+/////////////////////////////////
+var resultContainer1 = document.getElementById('daily');
+var searchButton = document.getElementById('search-forecast-button');
+
+searchButton.addEventListener('click', getApi1);
 
 
-  fetch(requestFiveDayUrl)
+function getApi1() {
+
+  var requestForecastUrl ='https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&cnt=40&appid=9f768ebe1fd823ba79ca13f691dcc877&units=imperial';
+
+  fetch(requestForecastUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data)  {
     console.log(data);  
-    });
-  }
+
+    // var cityName = document.createElement('h1'); //addClass / set Attribute to style to bold?
+    // const date = new Date();
+    // let day = date.getDate();
+    // let month = date.getMonth() + 1;
+    // let year = date.getFullYear();
+
+//let date = new Date().toLocaleDateString("de-DE"); get time in Germany
+// console.log(date); // 17.6.2022
+//https://saimana.com/list-of-country-locale-code/
+
+    // let currentDate = `${day}/${month}/${year}`;
+    // console.log(currentDate);
+    
+    var day1El = document.createElement('h5');
+
+    var tempForecastEl = document.createElement('p');
+    tempForecastEl.innerHTML =
+      '<strong>Temp:</strong> ' + data.list[0].main.temp + ' F/C';
+
+    var windForecastEl = document.createElement('p');
+    windForecastEl.innerHTML =
+      '<strong>Wind:</strong> ' + data.list[0].wind.speed + ' MPH';
+
+      var humidityForecastEl = document.createElement('p');
+    humidityForecastEl.innerHTML =
+      '<strong>Humidity:</strong> ' + data.list[0].main.humidity + ' %';
+
+    // cityName.textContent = data.name;
+    // cityTemp.textContent = data.main.temp;
+    // weatherDescription.textContent = data.wind.speed;s
+
+      // resultContainer.append(cityName);
+      // resultContainer.append(cityTemp);
+      // resultContainer.append(weatherDescription);
+
+      // resultContainer.append(cityName, currentDate);
+
+      day1El.textContent = data.list[0].dt_txt;
+      console.log();
+  
+      resultContainer1.append(day1El);
+      resultContainer1.append(tempForecastEl);
+      resultContainer1.append(windForecastEl);
+      resultContainer1.append(humidityForecastEl);
+  });
+}
+  
