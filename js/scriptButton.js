33b1c0f2d490c23aba334683b029a31cfb9f94b5
Connie Wong <conniehhw@gmail.com>
2023-01-17 16:07:32 -0500

    Add logic to create button based on search input & output in browser

--- js/scriptButton.js
+++ js/scriptButton.js
@@ -1,25 +1,69 @@
-// var historyContainer = document.querySelector('#history'); 
-// var searchFormEl = document.querySelector('#search-form-1');
+var historyContainer = document.querySelector('search-history'); 
+var searchFormEl = document.querySelector('#search-form-1');
 
-function addButton() {
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    var searchInputVal = document.querySelector('#search-input').value;
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+    console.log(searchInputVal);
+    
     var myDiv = document.getElementById("GFG");
-
     var button = document.createElement('BUTTON');
-
-    var text = document.createTextNode("Button");
-
+    var text = document.createTextNode(searchInputVal);
     button.appendChild(text);
+    myDiv.appendChild(button);   
+  }
 
-    myDiv.appendChild(button);
-}
+//   function addButton (event) {
+//     event.preventDefault();
+
+//     var myDiv = document.getElementById("GFG");
+//     var button = document.createElement('BUTTON');
+//     var text = document.createTextNode(searchInputVal);
+//     button.appendChild(text);
+//     myDiv.appendChild(button);
+//   }
+  searchFormEl.addEventListener('submit', handleSearchFormSubmit); 
+//   searchFormEl.addEventListener('submit', addButton); 
+
+
+// // function addButton() {
+// //     var myDiv = document.getElementById("GFG");
+
+// //     var button = document.createElement('BUTTON');
+
+// //     // var text = document.createTextNode("searchInputVal");
+
+// //     // button.appendChild(text);
+
+// //     myDiv.appendChild(button);
+// // }
+
+// // searchFormEl.addEventListener('submit', addButton); 
+
+// function addButton() {
+//     var myDiv = document.getElementById("GFG");
+//     var button = document.createElement('BUTTON');
+//     var text = document.createTextNode(searchInputVal);
+//     button.appendChild(text);
+//     myDiv.appendChild(button);
+// }
 
 // function handleSearchFormSubmit(event) {
 //     event.preventDefault();
 
 //     var searchInputVal = document.querySelector('#search-input').value;
-//         addButton(searchInputVal);
-// }
+  
+//     if (!searchInputVal) {
+//       console.error('You need a search input value!');
+//       return;
+//     } else { 
+//         addButton();
+//     }};
 
-// searchFormEl.addEventListener('submit', addButton); 
-
-
+//   searchFormEl.addEventListener('submit', handleSearchFormSubmit, addButton); 
